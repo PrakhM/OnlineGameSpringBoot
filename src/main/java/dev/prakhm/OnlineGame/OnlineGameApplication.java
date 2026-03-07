6fbc7314ea7e50bf144fceae04cac0d6ae81477b
Prakhar <realprakharm@gmail.com>
2026-03-07 11:53:01 +0530

    Initial commit, spring boot initialzr project with websocket

---
+++ src/main/java/dev/prakhm/OnlineGame/OnlineGameApplication.java
@@ -0,0 +1,13 @@
+package dev.prakhm.OnlineGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/prakhm/OnlineGame/OnlineGameApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prakhm.OnlineGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
